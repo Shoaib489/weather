b04a95c26ca4392166db368246edff7d9231b5d6
Shoaib489 <SHOAIB22657@GMAIL.COM>
2024-08-06 13:58:39 +0530

    first commit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,10 @@
 import Temperature from '@/components/temperature'
 import React from 'react'
-import RainAnimation from './boxData'
-import classes from "../pages/boxData/style.module.css"
 
 const index = () => {
   return (
-    // <div className={classes.mainBackG}>
-      <div className={classes.shadow}>
-        {/* <RainAnimation /> */}
+      
         <Temperature />
-      </div>
-    // </div>
   )
 }
 
